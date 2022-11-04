c7553b18ea1e421df24cecd9b8c652c72643a7a1
Melroy Noronha <melmax1208@gmail.com>
2022-11-04 17:35:59 +0530

    implement Auth0 login

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Button} from 'react-native';
+import {Auth0Provider, useAuth0} from 'react-native-auth0';
+import {AUTH0_DOMAIN, AUTH0_CLIENT_ID} from 'react-native-dotenv';
+
+const Home = () => {
+  const {authorize} = useAuth0();
+
+  const onPress = async () => {
+    try {
+      await authorize();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <Button onPress={onPress} title="Log in" />
+    </SafeAreaView>
+  );
+};
 
 const App: React.FC = () => {
-  return <SafeAreaView />;
+  return (
+    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
+      <Home />
+    </Auth0Provider>
+  );
 };
 
 export default App;
