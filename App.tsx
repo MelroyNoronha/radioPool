4a93de4562234a631e16a37058dcc3e91bed2be0
Melroy Noronha <melmax1208@gmail.com>
2022-11-04 18:59:52 +0530

    show user info

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Button, Text, Image, View} from 'react-native';
 import {Auth0Provider, useAuth0} from 'react-native-auth0';
 import {AUTH0_DOMAIN, AUTH0_CLIENT_ID} from 'react-native-dotenv';
 
 const Home = () => {
-  const {authorize, clearSession} = useAuth0();
+  const {authorize, clearSession, user} = useAuth0();
   const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setIsAuthorized(true);
+    } else {
+      setIsAuthorized(false);
+    }
+  }, [user]);
 
   const startLogin = async () => {
     try {
       await authorize();
-      setIsAuthorized(true);
     } catch (err) {
       console.log(err);
     }
@@ -19,16 +26,25 @@ const Home = () => {
   const startLogout = async () => {
     try {
       await clearSession();
-      setIsAuthorized(false);
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const UserProfile = ({username, imageUri}) => {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Image source={{uri: imageUri}} style={{height: 100, width: 100}} />
+        <Text>{username}</Text>
+      </View>
+    );
   };
 
   return (
     <SafeAreaView>
       {!isAuthorized && <Button onPress={startLogin} title="Log In" />}
       {isAuthorized && <Button onPress={startLogout} title="Log Out" />}
+      {user && <UserProfile username={user.name} imageUri={user.picture} />}
     </SafeAreaView>
   );
 };
