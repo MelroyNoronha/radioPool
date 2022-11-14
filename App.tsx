3dc06d85e125c6444e6e15b319438aedf740d888
Melroy Noronha <melmax1208@gmail.com>
2022-11-14 19:38:10 +0530

    install and configure react-native-app-auth

--- App.tsx
+++ App.tsx
@@ -1,59 +1,37 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Button, Text, Image, View} from 'react-native';
-import {Auth0Provider, useAuth0} from 'react-native-auth0';
-import {AUTH0_DOMAIN, AUTH0_CLIENT_ID} from 'react-native-dotenv';
+import React from 'react';
+import {SafeAreaView, Button} from 'react-native';
+import {SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET} from 'react-native-dotenv';
+import {authorize} from 'react-native-app-auth';
 
-const Home = () => {
-  const {authorize, clearSession, user} = useAuth0();
-  const [isAuthorized, setIsAuthorized] = useState(false);
+const App: React.FC = () => {
+  const signInToSpotify = async () => {
+    const config = {
+      clientId: SPOTIFY_CLIENT_ID,
+      clientSecret: SPOTIFY_CLIENT_SECRET,
+      redirectUrl: 'com.radiopool:/oauth', // the redirect you defined after creating the app
+      scopes: [
+        'user-read-email',
+        'playlist-modify-public',
+        'user-read-private',
+      ], // the scopes you need to access
+      serviceConfiguration: {
+        authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+        tokenEndpoint: 'https://accounts.spotify.com/api/token',
+      },
+    };
 
-  useEffect(() => {
-    if (user) {
-      setIsAuthorized(true);
-    } else {
-      setIsAuthorized(false);
-    }
-  }, [user]);
-
-  const startLogin = async () => {
     try {
-      await authorize();
+      const authState = await authorize(config);
+      const {accessToken, refreshToken} = authState;
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
-  };
-
-  const startLogout = async () => {
-    try {
-      await clearSession();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const UserProfile = ({username, imageUri}) => {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Image source={{uri: imageUri}} style={{height: 100, width: 100}} />
-        <Text>{username}</Text>
-      </View>
-    );
   };
 
   return (
     <SafeAreaView>
-      {!isAuthorized && <Button onPress={startLogin} title="Log In" />}
-      {isAuthorized && <Button onPress={startLogout} title="Log Out" />}
-      {user && <UserProfile username={user.name} imageUri={user.picture} />}
+      <Button onPress={signInToSpotify} title="Sign in to Spotify" />
     </SafeAreaView>
-  );
-};
-
-const App: React.FC = () => {
-  return (
-    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
-      <Home />
-    </Auth0Provider>
   );
 };
 
