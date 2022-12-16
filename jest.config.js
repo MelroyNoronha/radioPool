bae2545bf05a41efa86f813f40d0946a329eb9d8
Melroy Noronha <melmax1208@gmail.com>
2022-12-16 13:46:52 +0530

    add transformIgnorePatterns to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-base64|react-native-app-auth|react-native-encrypted-storage|@react-navigation)/)',
+  ],
 };
