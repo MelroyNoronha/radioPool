895e8b6e01fce093741b282dc5dc667fb4f77438
Melroy Noronha <melmax1208@gmail.com>
2023-01-20 18:29:02 +0530

    add redux-perist to transsformIgonrePatterns

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-native-base64|react-native-app-auth|react-native-encrypted-storage|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|react-native-base64|react-native-app-auth|react-native-encrypted-storage|@react-navigation|redux-persist)/)',
   ],
 };
