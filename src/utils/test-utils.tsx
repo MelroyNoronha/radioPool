8ad51d8692bfe19215b0d1bf0cb22370559d4fec
Melroy Noronha <melmax1208@gmail.com>
2023-01-20 18:29:46 +0530

    add redux-persist config to renderWithProviders test util

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -4,10 +4,21 @@ import type {RenderOptions} from '@testing-library/react-native';
 import {configureStore} from '@reduxjs/toolkit';
 import type {PreloadedState} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import type {AppStore, RootState} from '../redux/store';
-// As a basic setup, import your same slice reducers
-import authReducer from '../redux/auth/authSlice';
+import reducers from '../redux/reducers';
 
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as initialState, store.
@@ -16,17 +27,41 @@ interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   store?: AppStore;
 }
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const mockStore = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(mockStore);
+
 export function renderWithProviders(
   ui: React.ReactElement,
   {
     preloadedState = {},
     // Automatically create a store instance if no store was passed in
-    store = configureStore({reducer: {auth: authReducer}, preloadedState}),
+    store = mockStore,
     ...renderOptions
   }: ExtendedRenderOptions = {},
 ) {
   function Wrapper({children}: PropsWithChildren<{}>): JSX.Element {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>{children}</PersistGate>
+      </Provider>
+    );
   }
 
   // Return an object with the store and all of RTL's query functions
