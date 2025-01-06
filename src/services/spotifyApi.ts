7a58d83ac64fa78c69e9504a92317a29bc37d39e
MelroyNoronha <melmax1208@gmail.com>
2025-01-06 20:09:31 +0530

    Handle 401 status at base query level for spotifyApi

--- src/services/spotifyApi.ts
+++ src/services/spotifyApi.ts
@@ -1,6 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 import {RootState} from '../redux/store';
+import {removeUserSession} from '../redux/auth/authSlice';
 import {SPOTIFY_API_BASE_URL, SPOTIFY_PROFILE_ENDPOINT} from '../constants';
 
 interface SpotifyProfile {
@@ -30,18 +31,29 @@ interface SpotifyProfile {
   uri: string;
 }
 
+const baseQueryWithAuth = fetchBaseQuery({
+  baseUrl: SPOTIFY_API_BASE_URL,
+  prepareHeaders: (headers, {getState}) => {
+    const token = (getState() as RootState).auth.userSession.accessToken;
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
+  const response = await baseQueryWithAuth(args, api, extraOptions);
+  if (response.error && response.error.status === 401) {
+    api.dispatch(removeUserSession());
+  }
+  // TODO: Add a refresh token logic here
+  return response;
+};
+
 export const spotifyApi = createApi({
   reducerPath: 'spotifyApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: SPOTIFY_API_BASE_URL,
-    prepareHeaders: (headers, {getState}) => {
-      const token = (getState() as RootState).auth.userSession.accessToken;
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
   endpoints: builder => ({
     getSpotifyProfile: builder.query<SpotifyProfile, void>({
       query: () => ({url: SPOTIFY_PROFILE_ENDPOINT}),
