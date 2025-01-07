8737c232302c87525093767cce9ac59c8573223a
MelroyNoronha <melmax1208@gmail.com>
2025-01-08 00:12:31 +0530

    Upgrade react native to 0.71.15

--- ios/radioPool/AppDelegate.h
+++ ios/radioPool/AppDelegate.h
@@ -1,10 +1,8 @@
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
+#import <RCTAppDelegate.h>
 #import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
+@interface AppDelegate : RCTAppDelegate <RNAppAuthAuthorizationFlowManager>
 
-@property (nonatomic, strong) UIWindow *window;
 @property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @end
