517f7d54fbb7c360b3000aedf25fdd25605e8929
Melroy Noronha <melmax1208@gmail.com>
2025-07-01 21:16:09 +0530

    22 splash screen setup (#23)
    * Splash screen setup with react-native-bootsplash
    
    * Update images

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App: React.FC = () => {
-  return <SafeAreaView />;
+  useEffect(() => {
+    setTimeout(() => {
+      // check for auth token and navigate to auth or home
+      RNBootSplash.hide({ fade: true });
+    }, 2000);
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome to radioPool!</Text>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx'],
+        alias: {
+          '@/src': './src',
+          '@/components': './src/components',
+          '@/navigators': './src/navigators',
+          '@/screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
