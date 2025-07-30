b611856a49fc12c56a345da65b70d2e02e288e52
MelroyNoronha <melmax1208@gmail.com>
2025-07-30 22:14:13 +0530

    Add loading state to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,31 @@
-import { Pressable, PressableProps, StyleSheet } from 'react-native';
+import {
+  ActivityIndicator,
+  Pressable,
+  PressableProps,
+  StyleSheet,
+} from 'react-native';
 
 import { colors, fontSizes, spacings, ThemeValues } from '@/theme';
 import { Text } from '@/components';
 
 interface ButtonProps extends PressableProps {
   label: string;
+  isLoading?: boolean;
   currentTheme?: ThemeValues;
 }
 
 const Button = ({
   label,
   currentTheme = ThemeValues.light,
+  isLoading = false,
   ...otherProps
 }: ButtonProps) => {
   const styles = getStyles(currentTheme);
 
   return (
     <Pressable aria-label={label} style={styles.container} {...otherProps}>
-      <Text color="button">{label}</Text>
+      {!isLoading && <Text color="button">{label}</Text>}
+      {isLoading && <ActivityIndicator animating />}
     </Pressable>
   );
 };
